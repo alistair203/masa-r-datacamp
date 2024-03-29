7aba4daddd9d7d78d06df0fa88832bea99c24778
Alistair <alistair.fmy@gmail.com>
2024-03-29 23:22:39 +0800

    xddinside

---
+++ test.R
@@ -0,0 +1,14 @@
+x <- 3 + 4 
+x
+
+y = c(1:20)
+# UH theres like an option for R interactive terminal at th e bottom 
+
+#lookes like its working lol
+#but i dont hv access to the terminal lol
+# does it work now
+#lol
+#idk i didnt get one either
+# the graph pops up if i use the other terminal 
+
+df <- data.frame(matrix(nrow = 0, ncol= 17))
